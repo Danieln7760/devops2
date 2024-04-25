a60e062e4fa2ca82da916f7bfdc0d9ab7eaf43a6
Danieln7760 <92871147+Danieln7760@users.noreply.github.com>
2024-04-25 18:47:24 -0400

    Add files via upload

---
+++ project/SFHfoot.php
@@ -0,0 +1,7 @@
+<!-- Obinna worked on this page -->
+<FOOTER>
+
+	<DIV id="footer">
+	<P1>Virtual Gaming Hub is a registered trade-mark of no one in particular.</P1>
+	</DIV>
+	<FOOTER>
---
+++ project/SFHheader.php
@@ -0,0 +1,7 @@
+<!-- Obinna worked on this page -->
+<HEADER>
+
+	<div id="header">
+	<h1 id="logo">Sport Feed Hub</h1>
+	</div>
+</HEADER>
